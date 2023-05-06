cbb0a32f17fab8a5c52401af8c3464d7f0638c1a
erickbytes <erickrumbold@gmail.com>
2023-05-06 03:42:07 -0500

    added research cli option

--- README.md
+++ README.md
@@ -2,10 +2,36 @@
 
 finsou.py uses Python's html.parser + Beautiful Soup + regex to parse Yahoo price info.
 
-- parse Yahoo Finance HTML market close and after hours stock prices
-- download earnings reports w/ pdf_miner.py
-- default: prints summary
-- has optional csv command line argument
+- parse market close and after hours stock prices from Yahoo Finance HTML
+- print market open, daily price action, market close and adter hours summary for each stock
+- export prices to csv
+- download earnings reports and media
+
+Example Stock Summary
+```
+>>> python finsou.py -s "TSLA"
+
+TSLA
+
+200
+MONSTER BREAKOUT!
+Market Price $ Open: 161.2
+Regular Market $ Close: 170.06
+Daily % Change: +5.50%
+Daily $ Change: +8.86
+Post Market $ Close: 169.99
+After Hours % Change: -0.04%
+After Hours $ Change: -0.07
+https://finance.yahoo.com/quote/TSLA/
+```
+**CLI Options**
+
+| Arg  | Alt. Arg  | Description										     |
+|-----:|-----------|---------------------------------------------------------|
+|   -s | --stocks  | comma delited string of stocks   					     |
+|   -c | --csv     | write csv summary to given file, ex: "csv_name.csv"     |
+|   -r | --research| accepts invesor resources url, downloads PDF, docx, mp3 |
+|   -h | --help    | show help message and exit  						     |
 
 **Examples**
 ```
@@ -17,6 +43,10 @@ python finsou.py --stocks "TSLA,MSFT,AAPL" --csv "Prices Summary.csv"
 
 # Read a list of stocks from a text file with one ticker on each line.
 python finsou.py -s portfolio.txt -c "Portfolio Prices.csv"
+
+# Crawl an investor relations website for documents.
+python finsou.py --research https://investors.coca-colacompany.com/
+
 ```
 Example portfolio.txt contents:
 ```
@@ -24,11 +54,3 @@ TSLA
 MSFT
 AAPL
 ```
-
-**CLI Options**
-
-| Arg  | Alt. Arg  | Info     											  |
-|-----:|-----------|------------------------------------------------------|
-|   -s | --stocks  | comma delited string of stocks   					  |
-|   -c | --csv     | write csv summary to given file, ex: "csv_name.csv"  |
-|   -h | --help    | show help message and exit  						  |
--- finsou.py
+++ finsou.py
@@ -1,6 +1,8 @@
 import argparse
-import time
 import re
+import time
+import traceback
+import urllib
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -9,13 +11,14 @@ import pandas as pd
 def yahoo_finance_prices(url, stock):
     """Parse Yahoo Finance HTML price and after hours price info.
     Use Beautiful Soup + regex compile to select span tags with price values.
-    
-    The re.compile function accepts a str, class value to match if contained in any tag.
-    4 regex matches are used to get HTML tags and ( ) $ - are escaped with \
+
+    The re.compile function accepts a str, css class value to match if contained in any tag.
+    4 regex matches are used to get HTML tags with ( ) $ - escaped with \ in each.
     returns summary, str containing regular hours stock market + after hours prices
-    
+
     Beautiful Soup Docs:
     https://beautiful-soup-4.readthedocs.io/en/latest/#searching-by-css-class
+    https://docs.python.org/3/library/re.html#re.compile
     """
     # TODO: get after hours price in real time during after hour session
     headers = {
@@ -25,14 +28,11 @@ def yahoo_finance_prices(url, stock):
     r = requests.get(url, headers=headers)
     print(r.status_code)
     page = r.text
-    with open(f"{stock}.txt", "w") as f:
-        f.write(page)
     soup = BeautifulSoup(page, "html.parser")
     price_tags = soup.find_all(
         class_=re.compile("Fw\(b\) Fz\(36px\) Mb\(\-4px\) D\(ib\)")
     )
-    mkt_close_price = price_tags[0].string
-    mkt_close_price = mkt_close_price.replace(",", "")
+    mkt_close_price = price_tags[0].string.replace(",", "")
     price_change_tags = soup.find_all(class_=re.compile("Fw\(500\)"))
     price_change_tags = [span for span in price_change_tags if span.string is not None]
     price_change_tags = [
@@ -43,17 +43,14 @@ def yahoo_finance_prices(url, stock):
     daily_price_change = price_change_tags[0].replace("(", "").replace(")", "").strip()
     daily_pct_change = price_change_tags[1].replace("(", "").replace(")", "").strip()
     percent = (
-        daily_pct_change.replace("-", "")
-        .replace("+", "")
-        .replace("(", "")
-        .replace(")", "")
-        .replace("%", "")
-        .strip()
+        daily_pct_change.replace("-", "").replace("+", "").replace("%", "").strip()
     )
     info = list()
-    # Display message if stonk had a big daily move.
+    # Display message if stpck had a big daily move.
     if "+" in daily_pct_change and float(percent) > 4:
-        info.append("MONSTER BREAKOUT DAY!")
+        info.append("MONSTER BREAKOUT!")
+    elif "+" in daily_pct_change and float(percent) >= 1 and float(percent) <= 4:
+        info.append("SOLID GREEN DAY!")
     elif "-" in daily_pct_change and float(percent) > 4:
         info.append("SELL-OFF ALERT!")
     post_mkt_tags = soup.find_all(class_=re.compile("Mstart\(4px\)"))
@@ -103,15 +100,51 @@ def yahoo_finance_prices(url, stock):
     return summary
 
 
+def research(url):
+    """Parse HTML with Python's html.parser and bs4.
+    Select all nodes with an href value, print the urls and
+    filter the file object urls with list comprehensions for download.
+
+    The urllib urlretrieve function copies a network object to local file.
+    The second argument, if present, specifies the file location to copy to.
+    If filename is absent, the location will be a tempfile with a generated name.
+
+    https://docs.python.org/3/library/urllib.request.html#urllib.request.urlretrieve
+    """
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, "html.parser")
+    urls = [node.get("href") for node in soup.find_all(href=True)]
+    pdfs = [url for url in urls if url.endswith(".pdf")]
+    csvs = [url for url in urls if url.endswith(".csv")]
+    mp4s = [url for url in urls if url.endswith(".mp4")]
+    docs = [url for url in urls if url.endswith(".docx")]
+    xls = [url for url in urls if url.endswith(".xlsx")]
+    objects = pdfs + csvs + mp4s + docs + xls
+    if len(objects) == 0:
+        print("No media found to download.")
+        return None
+    for obj_url in objects:
+        try:
+            obj = obj_url.split("/")[-1]
+            urllib.request.urlretrieve(url, filename=obj)
+            print(f"New network object saved: {obj}")
+        except urllib.error.HTTPError:
+            traceback.print_exc()
+            print(f"Forbidden to download file: {obj}")
+        except ValueError:
+            traceback.print_exc()
+            print(f"Failed due to invalid url: {obj}")
+        return None
+
+
 parser = argparse.ArgumentParser(
     prog="finsou.py",
-    description="Summarize prices from Yahoo website.",
+    description="Beautiful Financial Soup",
     epilog="fin soup... yum yum yum yum",
 )
-parser.add_argument(
-    "-s", "--stocks", help="comma delited string of stocks or portfolio.txt"
-)
-parser.add_argument("-c", "--csv", help='"CSVNAME.csv"')
+parser.add_argument("-s", "--stocks", help="comma sep. stocks or portfolio.txt")
+parser.add_argument("-c", "--csv", help='set csv export with "your_csv.csv"')
+parser.add_argument("-r", "--research", help="accepts investor relations website url")
 args = parser.parse_args()
 if ".txt" in args.stocks:
     with open(args.stocks, "r") as f:
@@ -137,5 +170,5 @@ for stock in stocks:
     # Added time delay between each request to avoid too many hits too fast.
     time.sleep(2)
 if args.csv:
-    stock_prices = pd.DataFrame(prices, columns=["Stock", "Post_Market_Close", "URL"])
+    stock_prices = pd.DataFrame(prices, columns=["Stock", "Price_Summary", "URL"])
     stock_prices.to_csv(args.csv, index=False)
